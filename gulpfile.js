5f5c0de6d39b916d71a9eed4a2e1f6666b0f6e20
sourabh <sourabh8003@gmail.com>
2018-07-19 01:55:28 +0530

    Update gulp config for build diectory

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     del = require('del'),
     flatten = require('gulp-flatten');
-    
+
 gulp.task('build-css', function() {
 	gulp.src([
         'src/app/components/common/common.css',
 		    'src/app/components/**/*.css'
     ])
         .pipe(concat('primeng.css'))
-        .pipe(gulp.dest('resources'));
+        .pipe(gulp.dest('build/resources'));
 });
 
 gulp.task('build-css-prod', function() {
@@ -22,41 +22,40 @@ gulp.task('build-css-prod', function() {
         'src/app/components/**/*.css'
     ])
     .pipe(concat('primeng.css'))
-    .pipe(gulp.dest('resources'))
+    .pipe(gulp.dest('build/resources'))
     .pipe(uglifycss({"uglyComments": true}))
     .pipe(rename('primeng.min.css'))
-    .pipe(gulp.dest('resources'));	
+    .pipe(gulp.dest('build/resources'));
 });
 
 gulp.task('copy-component-css', function () {
     gulp.src([
         'src/app/components/**/*.css'
     ])
-    .pipe(gulp.dest('resources/components'));
+    .pipe(gulp.dest('build/resources/components'));
 });
 
 gulp.task('images', function() {
     return gulp.src(['src/app/components/**/images/*.png', 'src/app/components/**/images/*.gif'])
         .pipe(flatten())
-        .pipe(gulp.dest('resources/images'));
+        .pipe(gulp.dest('build/resources/images'));
 });
 
 gulp.task('themes', function() {
     return gulp.src(['src/assets/components/themes/**/*'])
-        .pipe(gulp.dest('resources/themes'));
+        .pipe(gulp.dest('build/resources/themes'));
 });
 
 gulp.task('build-exports', function() {
     return gulp.src(['exports/*.js','exports/*.d.ts'])
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('build/'));
 });
 
 //Cleaning previous gulp tasks from project
 gulp.task('clean', function() {
-	del(['resources']);
+	del(['build/resources']);
 });
 
 //Building project with run sequence
 gulp.task('build-assets', ['clean','copy-component-css', 'build-css-prod', 'images', 'themes']);
 
-        
